63ce67d4a6ace51c340c9ba0d52ba3c9224a0a3d
Steven Oud <soud@protonmail.com>
2018-11-13 13:53:08 +0100

    Fix PathVariable

--- src/main/java/com/aquascore/api/controllers/TeamController.java
+++ src/main/java/com/aquascore/api/controllers/TeamController.java
@@ -1,21 +1,12 @@
 package com.aquascore.api.controllers;
 
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-
 import com.aquascore.api.models.Team;
 import com.aquascore.api.services.TeamService;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @RestController
 @RequestMapping("/teams/")
@@ -29,17 +20,17 @@ public class TeamController {
     }
 
     @PostMapping("/")
-    public Team create(@RequestBody Team newTeam){
+    public Team create(@RequestBody Team newTeam) {
         return teamService.create(newTeam);
     }
 
     @PatchMapping("/{team_id}")
-    public Team edit(@RequestBody Team newTeam, @PathVariable("team_id") long teamId){
+    public Team edit(@RequestBody Team newTeam, @PathVariable("team_id") long teamId) {
         return teamService.edit(newTeam, teamId);
     }
 
     @DeleteMapping("/{team_id}")
-    public void remove(@PathVariable("{team_id}") long teamId){
+    public void remove(@PathVariable("team_id") long teamId) {
         teamService.remove(teamId);
     }
 
