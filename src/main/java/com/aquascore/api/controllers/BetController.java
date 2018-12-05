8e31ebe5419fd60dae61275a1f35aab795b1c54b
Yannick <yannickdg99@gmail.com>
2018-12-05 15:04:44 +0100

    Start on backend

--- src/main/java/com/aquascore/api/controllers/BetController.java
+++ src/main/java/com/aquascore/api/controllers/BetController.java
@@ -1,6 +1,8 @@
 package com.aquascore.api.controllers;
 
 import com.aquascore.api.models.Bet;
+import com.aquascore.api.models.CurrentBet;
+import com.aquascore.api.services.CurrentBetService;
 import com.aquascore.api.services.BetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -12,20 +14,29 @@ import java.util.List;
 @RequestMapping("/bets/")
 public class BetController {
     @Autowired
+    CurrentBetService currentBetService;
+
+    @Autowired
     BetService betService;
 
     @GetMapping("/")
-    public List<Bet> getAll(HttpServletRequest req) {
+    public List<Bet> getAllBets(HttpServletRequest req) {
         return betService.getAll();
     }
+    
+    @GetMapping("/{userId}")
+    public List<CurrentBet> getAllUserBets(HttpServletRequest req) {
+        return currentBetService.getAll();
+    }
+
 
     @PostMapping("/")
-    public Bet create(@RequestBody Bet newBet) {
-        return betService.create(newBet);
+    public CurrentBet create(@RequestBody CurrentBet newBet) {
+        return currentBetService.create(newBet);
     }
 
     @DeleteMapping("/{betId}")
     public void remove(@PathVariable("betId") long betId) {
-        betService.remove(betId);
+        currentBetService.remove(betId);
     }
 }
