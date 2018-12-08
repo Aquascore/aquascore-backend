fc92fad59b79a026adf10a678f423b0e5510811e
Leon Timmerman <leontimmerman123@gmail.com>
2018-12-08 15:38:11 +0100

    Changes to delete method, to delete divers when a team gets deleted

--- src/main/java/com/aquascore/api/services/DriverService.java
+++ src/main/java/com/aquascore/api/services/DriverService.java
@@ -3,43 +3,64 @@ package com.aquascore.api.services;
 import java.util.List;
 
 import com.aquascore.api.models.Driver;
+import com.aquascore.api.models.Team;
 import com.aquascore.api.repositories.DriverRepository;
+import com.aquascore.api.repositories.TeamRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class DriverService{
+public class DriverService {
     @Autowired
     private DriverRepository driverRepository;
 
-    public Driver getById(long id){
+    @Autowired
+    private TeamRepository teamRepository;
+
+    public Driver getById(long id) {
         return driverRepository.findById(id);
     }
 
-    public List<Driver> getAll(){
+    public List<Driver> getAll() {
         return driverRepository.findAll();
     }
 
-    public Driver create(Driver newDriver){
+    public Driver create(Driver newDriver) {
         driverRepository.save(newDriver);
         return newDriver;
     }
 
-    public Driver edit(Driver newDriver, long driverId){
+    public Driver edit(Driver newDriver, long driverId) {
         Driver oldDriver = driverRepository.findById(driverId);
 
         oldDriver.setFirstname(newDriver.getFirstname());
         oldDriver.setLastname(newDriver.getLastname());
         oldDriver.setSalary(newDriver.getSalary());
-        oldDriver.setTeamid(newDriver.getTeamid());
+
+        if (oldDriver.getTeamid() != newDriver.getTeamid()) {
+            Team oldTeam = teamRepository.findById(oldDriver.getTeamid());
+            Team newTeam = teamRepository.findById(newDriver.getTeamid());
+
+            oldDriver.setTeamid(newDriver.getTeamid());
+
+            oldTeam.removeMember(oldDriver);
+            newTeam.addMember(oldDriver);
+
+            teamRepository.save(oldTeam);
+            teamRepository.save(newTeam);
+        }
 
         driverRepository.save(oldDriver);
         return oldDriver;
     }
 
-    public void remove(long driverId){
+    public void remove(long driverId) {
         Driver driver = driverRepository.findById(driverId);
+        Team currentTeam = teamRepository.findById(driver.getTeamid());
+        currentTeam.removeMember(driver);
+
+        teamRepository.save(currentTeam);
         driverRepository.delete(driver);
     }
 }
